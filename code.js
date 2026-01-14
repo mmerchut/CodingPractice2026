5412335a0f40f41a7ab2b134ff155ecd4de88234
Weronika <weronika@MacBook-Air-Weronika.local>
2026-01-14 21:48:26 +0100

    14.01 2.0

--- code.js
+++ code.js
@@ -1,1 +1,21 @@
-123
+//
+
+function playerRankUp (points) {
+  if (points >= 100) {
+    return "Well done! You have advanced to the qualifying stage. Win 2 out of your next 3 games to rank up."
+  } else {
+    return false
+  }
+}
+
+//
+
+function sum (numbers) {
+   if (numbers.length === 0) {
+        return 0;
+    }
+
+    return numbers.reduce((sum, current) => sum + current, 0);
+}
+
+//
